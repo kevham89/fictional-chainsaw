5c661391131a6a4b64acfc4edde2f84d34398257
kevham89 <kevin.hammarstedt@gmail.com>
2026-03-24 13:54:28 +0100

    test

--- lista.py
+++ lista.py
@@ -1,16 +1,3 @@
-fåglar = ["Sparv", "Skata", "Törn", "Koltrast", "Nötskrika", "Blåmes", "Gråsparv", "Stare", "Gök", "Kaja"]
-fiskar = ["Abborre", "Gädda", "Sik", "Lax", "Röding", "Öring", "Sill", "Torsk", "Makrill", "Hälleflundra"]
-hästar = ["Arab", "Fjord", "Islandshäst", "Lippizaner", "Morgan", "Mustang", "Quarterhäst", "Shire", "Shetlandsponny", "Tinker"]
-hundar = ["Labrador", "Golden Retriever", "Schäfer", "Bulldog", "Pudel", "Beagle", "Boxer", "Rottweiler", "Dachshund", "Siberian Husky"]
-
-for x in fåglar:
-    print(x)
-
-for x in fiskar:
-    print(x)
-
-for x in hästar:
-    print(x)
-
-for x in hundar:
-    print(x)
+tal = [1,2,3,4]
+for x in tal:
+    print(x * tal)
